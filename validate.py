385722009001a8485d53a32f0d0ed22ac0eb9188
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-22 12:15:57 +0100

    Included list pargument

--- validate.py
+++ validate.py
@@ -10,17 +10,21 @@ def main(args=None):
     ### Parsing options
     parser = ArgumentParser(usage="Usage: %(prog)s [options]", description="nTuple to TTree converter")
     
-    parser.add_argument("-i","--input", type=str, dest='input', help='ROOT MC ROOT files directory')
-    parser.add_argument("-o","--output", type=str, dest='output', default="MCheck.root" , help='name of output root TFile')
+    parser.add_argument("-i","--input", type=str, dest='input', help='MC ROOT input directory')
+    parser.add_argument("-l","--list", type=str, dest='list', help='MC ROOT input file list')
+    #arser.add_argument("-o","--output", type=str, dest='output', default="MCheck.root" , help='name of output root TFile')
+    parser.add_argument("-o","--output", type=str, dest='output', help='name of output root TFile')
     parser.add_argument("-v","--verbose", dest='verbose', default=False, action='store_true', help='run in high verbosity mode')
     parser.add_argument("-d","--debug", type=int, dest='debug', const=1000, nargs='?', help='activate debug mode - const value 1000 events')
 
     opts = parser.parse_args(args)
-
+    
     #Load analysis functions
     sys.path.append("moduls")
     from kompressor import analyzeMC
-
+    from stuff import createOutDir
+    
+    # Start analysis routine
     analyzeMC(opts)
 
 if __name__ == '__main__':
