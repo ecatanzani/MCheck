5490e6e773af441c0dc34770e10c4c45582cb640
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-23 00:01:23 +0100

    Create HTCondor job directories
    Add jobs directories and sub files

---
+++ Assets/condorHelper/moduls/createCondorDirs.py
@@ -0,0 +1,31 @@
+import os
+from subFileWriter import createSubFiles
+
+
+def createJobsDirs(opts, nDirs, outDir):
+
+    if opts.verbose:
+        print("Creating HTCondor output jobs dirs...")
+    homeWD = os.getcwd()
+    os.chdir(outDir)
+    condorDirs = []
+    for iDir in range(nDirs):
+        tmpDirName = outDir + "/" + "job_" + str(iDir)
+        if not os.path.isdir(tmpDirName):
+            try:
+                os.mkdir(tmpDirName)
+            except OSError:
+                print('Creation of the output directory {} failed'.format(tmpDirName))
+                raise
+            else:
+                condorDirs.append(tmpDirName)
+                if opts.verbose:
+                    print('Succesfully created output directory: {}'.format(tmpDirName))
+        else:
+            condorDirs.append(tmpDirName)
+            if opts.verbose:
+                print('Using existing output path directory: {}'.format(tmpDirName))
+    os.chdir(homeWD)
+
+    # Create HTCondor sub file
+    createSubFiles(opts, condorDirs)
