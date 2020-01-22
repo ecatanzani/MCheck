960b5c9be5e7e55097f4cce204972a9f0a0849d9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-22 21:35:40 +0100

    Removed a blank row at the begin of each out list

--- Assets/condorHelper/moduls/writeList.py
+++ Assets/condorHelper/moduls/writeList.py
@@ -11,8 +11,11 @@ def writeListToFile(opts, dataList, outDir, listIdx, singleListOutput=None):
         print('Creation of the output data list {} failed'.format(listPath))
         raise
     else:
-        for file in dataList:
-            outList.write("\n%s" % file)
+        for idx, file in enumerate(dataList):
+            if idx == 0:
+                outList.write(file)
+            else:
+                outList.write("\n%s" % file)
         outList.close()
         if opts.verbose:
             print('Created output list: {}'.format(listPath))
