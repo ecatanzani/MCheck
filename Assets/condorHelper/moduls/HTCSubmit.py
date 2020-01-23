c5c4d7a0c6062d148cfe140af880eee7faab1f23
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-23 09:52:30 +0100

    Setting correct DAMPE env

--- Assets/condorHelper/moduls/HTCSubmit.py
+++ Assets/condorHelper/moduls/HTCSubmit.py
@@ -3,5 +3,7 @@ import subprocess
 
 def submitJobs(opts, condorDirs):
     for folder in condorDirs:
+        subprocess.run("setDAMPE")
+        subprocess.run("dampe_init")
         subprocess.run(
             ["condor_submit", "-name sn-01.cr.cnaf.infn.it -spool crawler.sub"])
