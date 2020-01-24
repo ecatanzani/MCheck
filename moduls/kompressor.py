7e8b8643593bd879a0985faa91eff99d34318655
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-24 12:13:33 +0100

    Code refactory and removed unused moduls

--- moduls/kompressor.py
+++ moduls/kompressor.py
@@ -1,14 +1,13 @@
-import sys
-
-### Load ROOT modules
-from ROOT import TFile,TH1D,gROOT,gSystem
+# Load ROOT modules
+from ROOT import TH1D, gROOT, gSystem
 
 # Import TTree branching module
-from inspectData import readMC,addToChain
+from inspectData import readMC, addToChain
 from stuff import createOutROOTFile
 
+
 def analyzeMC(opts):
-    
+
     # Load DAMPE libs
     gSystem.Load("libDmpEvent.so")
     gSystem.Load("libDmpKernel.so")
@@ -19,30 +18,28 @@ def analyzeMC(opts):
 
     # Set ROOT verbosity
     gROOT.SetBatch(True)
-    gROOT.ProcessLine("gErrorIgnoreLevel = %i;"%opts.verbose)
+    gROOT.ProcessLine("gErrorIgnoreLevel = %i;" % opts.verbose)
 
     # Create output TFile
     outFile = createOutROOTFile(opts)
-    
+
     # Create eKin energy histo
     nBins = 10000
     xLow = 0
     xUp = 200000
-    eKinHisto = TH1D("eKinHisto","MC Trye Kinetic Energy",nBins,xLow,xUp)
-    
-    #Creating DAMPE chain for input files
+    eKinHisto = TH1D("eKinHisto", "MC Trye Kinetic Energy", nBins, xLow, xUp)
+
+    # Creating DAMPE chain for input files
     dmpch = DmpChain("CollectionTree")
 
     # Add ROOT files to the chain and get total events
-    nevents = addToChain(opts,dmpch)
-    
+    nevents = addToChain(opts, dmpch)
+
     # Analyze events
-    readMC(opts,dmpch,nevents,eKinHisto,kStep=1e+4)
+    readMC(opts, dmpch, nevents, eKinHisto, kStep=1e+4)
 
     # Write histo to TFile
     eKinHisto.Write()
 
     # Write output TFile
     outFile.Write()
-
-    
